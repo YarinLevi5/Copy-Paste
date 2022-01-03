76a475d5798dcdf5577341691cac7ff17df586d3
yarin <yarin066@gmail.com>
2022-01-03 22:43:51 +0200

    Animation for the process

--- copy.js
+++ copy.js
@@ -1,12 +1,14 @@
-        $('.copy-btn').click(function () {
-            $('.box .content-field').select();
-            document.execCommand('copy');
-            setTimeout(() => {
-                $('.box').toggleClass('hide');
-                $('.msg-box').addClass('show');
-                $(".content-msg").text("Line selected").
-                show().fadeOut(2000);
-                $('.msg-box').removeClass('show');
-                $('.paste-box').addClass('show');
-            }, 3000);
-        });
+$('.copy-btn').click(function () {
+    $('.box .content-field').select();
+    document.execCommand('copy');
+
+    $('.box').toggleClass('hide');
+    $('.msg-box').addClass('show');
+    $(".content-msg").text("Line selected").
+    show().fadeOut(2000);
+
+    setTimeout(() => {
+        $('.msg-box').removeClass('show');
+        $('.paste-box').addClass('show');
+    }, 2000);
+});
